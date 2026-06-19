aca4041db2101005f39804843f5486ee56bd156b
yinkscss <olayinkashittu2003@gmail.com>
2026-06-19 14:05:49 +0100

    Move matchMedia mock into global Vitest setup

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,2 +1,16 @@
 // Vitest global test setup: extends expect with jest-dom matchers
 import "@testing-library/jest-dom";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => undefined,
+    removeListener: () => undefined,
+    addEventListener: () => undefined,
+    removeEventListener: () => undefined,
+    dispatchEvent: () => false,
+  }),
+});
